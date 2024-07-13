98ac0ffe1bb50970822f7ab3e23d90e0b6271ddc
Martim Moreira <98058202+Patoinks@users.noreply.github.com>
2024-07-13 15:04:19 +0100

    Update 1

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+require('dotenv').config(); // Load environment variables from .env file
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-// Replace 'YOUR_BOT_TOKEN' with your bot's token
-const token = 'MTI2MTY2NzA3NjgxMzIyNTk4NA.Ga3iKR.PhYMMMgyHtW-QbV06X7XSLWoRQRvkTFsoIooEU';
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const token = process.env.BOT_TOKEN; // Access token from environment variable
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', message => {
-    if (message.content === '!ping') {
-        message.channel.send('Pong!');
+    if (message.content === '/drip') {
+        message.channel.send('otário');
     }
 });
 
